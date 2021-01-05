cc841b5f01034ab8695adf45ed954b285531dd62
Gabriel Ramos dos Santos <gabrielramosds@outlook.com>
2021-01-05 19:55:50 -0300

    Primeiro commit

---
+++ convertImageCartoon.py
@@ -0,0 +1,25 @@
+"""
+Convertendo qualquer imagem, para uma imagem "cartoonizada"
+"""
+
+import cv2
+
+# Lendo a imagem
+img = cv2.imread("lobo.png")
+
+# Editando arestas
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+edges = cv2.adaptiveThreshold(gray, 255,
+                              cv2.ADAPTIVE_THRESH_MEAN_C,
+                              cv2.THRESH_BINARY, 9, 9)
+
+# Cartoonizando
+color = cv2.bilateralFilter(img, 9, 250, 250)
+cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+cv2.imshow("Image", img)
+cv2.imshow("edges", edges)
+cv2.imshow("Cartoon", cartoon)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
